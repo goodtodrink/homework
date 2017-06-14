dc477eaaf62929cafa649b7e2b3fe3305e314c47
asus\user <justtaco0121@gmail.com>
2017-06-14 22:50:47 +0800

    no message

--- w9hw1.php
+++ w9hw1.php
@@ -1,19 +1,8 @@
-<form action="w9hw2.php" method="post">
-	請選擇要上傳的檔案數量<br>
-	<select name="size">
-		<option value="1">1</option>
-		<option value="2">2</option>
-		<option value="3">3</option>
-		<option value="4">4</option>
-		<option value="5">5</option>
-		<option value="6">6</option>
-		<option value="7">7</option>
-		<option value="8">8</option>
-		<option value="9">9</option>
-		<option value="10">10</option>
-
-	</select>
-	<br>
-	<br>
+<form action="w9hw2.php" method="post" enctype="multipart/form-data">
+		<?php 
+			$num=$_POST["num"];
+			for ($i=0; $i < $num ; $i++) { ?>
+		 	<input type="file" name="upload[]"><br>
+		 <?php } ?>
 	<input type="submit" value="送出">
 </form>
--- w9hw2.php
+++ w9hw2.php
@@ -1,48 +1,18 @@
-<?php  
+<?php
+	$j=count($_FILES["upload"]["name"]);
+	for ($i=0; $i < $j; $i++) { 
+		if (isset($_FILES["upload"]["name"][$i])) {
+			echo $_FILES["upload"]["name"][$i]."<br/>";
+			echo $_FILES["upload"]["tmp_name"][$i]."<br/>";
+			echo $_FILES["upload"]["size"][$i]."<br/>";
+			echo $_FILES["upload"]["type"][$i]."<br/>";
+		
+			$a=pathinfo($_FILES["upload"]["name"][$i]);
+			$t=time();
 
-	if (isset($_POST["size"])) 
-	{
-		$num = $_POST["size"];
-		echo "<form action='' method='post' enctype='multipart/form-data'>";
-		echo "請選擇上傳檔案";
-		for ($i=0; $i<$num; $i++) 
-		{ 
-			echo "<input type='file' name='file[]'><br><br>";	
-		}
-		echo "<input type='submit' value='上傳'>";
-		echo "&nbsp;";
-		echo "&nbsp;";
-		echo "<a href='w9hw1.php'>";
-		echo "<input type='button' value='回上頁'>";
-		echo "</a>";
-		echo "</form>";
+			copy($_FILES["upload"]["tmp_name"][$i],$t.".".$a["extension"]);
+		}else{
+			echo "上傳失敗";
+		}	
 	}
-
-	if (isset($_FILES['file'])) 
-	{
-
-		$i=count($_FILES["file"]["name"]); 
-   		for ($j=0 ; $j<$i ; $j++) 
-   		{ 
-      		copy($_FILES["file"]["tmp_name"][$j], $_FILES["file"]["name"][$j]);
-      		if (copy($_FILES["file"]["tmp_name"][$j], $_FILES["file"]["name"][$j]))
-			{
-				if ($j==($i-1)) 
-				{
-					echo "上傳成功" . "<br><br>";
-					echo "<a href='w9hw1.php'>";
-					echo "<input type='button' value='回上頁'>";
-					echo "</a>";
-				}
-			}
-			else
-			{
-				echo "上傳失敗" . "<br><br>";
-				echo "<a href='w9hw1.php'>";
-				echo "<input type='button' value='回上頁'>";
-				echo "</a>";
-			}
-   		}
-
-   	}
 ?>
