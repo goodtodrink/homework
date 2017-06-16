6f81a3ad4a38fbd659587c09b64de14cb9a4e775
asus\user <justtaco0121@gmail.com>
2017-06-16 19:56:19 +0800

    fix w9hw

--- w9hw1.php
+++ w9hw1.php
@@ -1,8 +1,10 @@
 <form action="w9hw2.php" method="post" enctype="multipart/form-data">
 		<?php 
 			$num=$_POST["num"];
+			
 			for ($i=0; $i < $num ; $i++) { ?>
 		 	<input type="file" name="upload[]"><br>
+		 	
 		 <?php } ?>
 	<input type="submit" value="送出">
 </form>
--- w9hw2.php
+++ w9hw2.php
@@ -2,17 +2,18 @@
 	$j=count($_FILES["upload"]["name"]);
 	for ($i=0; $i < $j; $i++) { 
 		if (isset($_FILES["upload"]["name"][$i])) {
-			echo $_FILES["upload"]["name"][$i]."<br/>";
-			echo $_FILES["upload"]["tmp_name"][$i]."<br/>";
-			echo $_FILES["upload"]["size"][$i]."<br/>";
-			echo $_FILES["upload"]["type"][$i]."<br/>";
-		
-			$a=pathinfo($_FILES["upload"]["name"][$i]);
-			$t=time();
+			echo "檔案名稱:".$_FILES["upload"]["name"][$i]."<br/>";
+			echo "暫存檔名:".$_FILES["upload"]["tmp_name"][$i]."<br/>";
+			echo "檔案尺寸:".$_FILES["upload"]["size"][$i]."<br/>";
+			echo "檔案類型:".$_FILES["upload"]["type"][$i]."<br/>";
+			echo "上傳成功!!<br/>";
+			echo "<br/>";
+			$name=pathinfo($_FILES["upload"]["name"][$i]);
+			$time=time();
 
-			copy($_FILES["upload"]["tmp_name"][$i],$t.".".$a["extension"]);
+			copy($_FILES["upload"]["tmp_name"][$i],$time.".".$name["extension"]);
 		}else{
-			echo "上傳失敗";
+			echo "上傳失敗!!";
 		}	
 	}
 ?>
